84c5009d2ef493eb4c96dbb4d9ad52bde753eada
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-03-13 18:31:07 +0200

    configure project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+// core
+import React from 'react';
+
+// library
+import {Switch, Route, Redirect} from 'react-router-dom';
+
+// components
+import {Home} from "../Pages";
+import {Header} from "../common";
+import {routes} from './routes';
+
+//styles
+import styles from './App.module.scss';
+
+export const App = () => {
+    return (
+        <div className={styles.app}>
+            <Header/>
+            <main className={styles.main}>
+                <Switch>
+                    <Redirect exact from='/' to={routes.home}/>
+                    <Route path={routes.home} component={Home}/>
+                </Switch>
+            </main>
+        </div>
+    );
+};
---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+// core
+import React from 'react';
+
+// styles
+import styles from './Home.module.scss';
+
+export const Home = () => {
+    return (<div className={styles.home}>Home</div>);
+};
